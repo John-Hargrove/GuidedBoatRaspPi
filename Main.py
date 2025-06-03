730a4b24065fc466ecba6c91f9766958c665cc17
John-Hargrove <Johnh6502@outlook.com>
2025-06-03 10:50:54 -0700

    new

--- Main.py
+++ Main.py
@@ -4,16 +4,25 @@ import GpsModule
 import requests
 import json
 
+import adafruit_gps
+import board
+import time
+import busio
+
 from flask import Flask, Response, Request
 import cv2 #from opencv-python
 
 #https://randomnerdtutorials.com/raspberry-pi-pinout-gpios/ raspberry pi pinout
-
+# GPS Pin --> Raspberry Pi Pin
+# VIN --> 4 (5V)
+# GND --> 6 (GND)
+# TX --> 10 (GPIO15, RXD)
+# RX --> 8 (GPIO14, TXD)
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)  # 0 = first USB camera
 
 def main():
-    foo = 2
+    foo = 0
 
 if __name__ == "__main__":
     main()
