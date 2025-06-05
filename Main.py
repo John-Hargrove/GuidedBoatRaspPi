4b642ffd6c4f3b117a02f9e10c10840948edcdeb
John-Hargrove <Johnh6502@outlook.com>
2025-06-05 13:26:13 -0700

    new

--- Main.py
+++ Main.py
@@ -5,6 +5,8 @@
 # TX --> 10 (GPIO15, RXD)
 # RX --> 8 (GPIO14, TXD)
 
+import time
+
 import serial
 import adafruit_gps
 
@@ -81,7 +83,23 @@ def post_gps_data():
 app.run(host='0.0.0.0', port=5000)
 
 def main():
-    foo = 0
+    last_print = time.monotonic()
+
+    while True:
+        gps.update()
+        current = time.monotonic()
+        if current - last_print >= 1.0:
+            last_print = current
+            if not gps.has_fix:
+                print("Waiting for fix...")
+                continue
+
+            print("=" * 40)
+            print("Fix timestamp: {}".format(gps.timestamp_utc))
+            print("Latitude: {:.6f} degrees".format(gps.latitude))
+            print("Longitude: {:.6f} degrees".format(gps.longitude))
+            print("Fix quality: {}".format(gps.fix_quality))
+            print("Satellites: {}".format(gps.satellites))
 
 if __name__ == "__main__":
     main()
--- ServerHost.py
+++ ServerHost.py
@@ -1,6 +1,3 @@
-# @app.route("/")
-# def index():
-#     return ("<h1>Live USB Camera Stream</h1><img src='/video'>"
-#             "<h2>Test<h/2>")
+
 
 
